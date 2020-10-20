1b9dcf2ce688d0d3292fc336d91ccb2edfba6fd1
Nasia <gamepichaya2@gmail.com>
2020-10-20 15:53:03 +0700

    nasia

---
+++ src/gittest/Gittest.java
@@ -0,0 +1,21 @@
+
+package gittest;
+
+/**
+ *
+ * @author Student
+ */
+public class Gittest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        System.out.println("Hello");
+        
+    }
+}
+
+    
+
